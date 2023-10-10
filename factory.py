f38009ffb1ffc0f1d8c024ebabb2e28efd7e0556
KaahinAtNTI <abdirahman.mohamed@ga.ntig.se>
2023-10-10 13:57:27 +0200

    first init

---
+++ factory.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+from blueprints.home import home_bp
+from blueprints.about import about_bp
+from blueprints.contact import contact_bp
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register blueprints
+    app.register_blueprint(home_bp)
+    app.register_blueprint(about_bp)
+    app.register_blueprint(contact_bp)
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+    return app
