73672d11c3277208b3d4883a4746915d9015ea38
KaahinAtNTI <abdirahman.mohamed@ga.ntig.se>
2023-10-11 10:05:49 +0200

    Update: CMS included

--- blueprints/about.py
+++ blueprints/about.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
+from blueprints.data.about_entry import about_entries
 
 about_bp = Blueprint("about", __name__)
 
 
 @about_bp.route("/about", methods=["GET"])
 def about():
-    return render_template("about.html")
+    active_entry = None
+    for entry in about_entries:
+        if entry["isActive"]:
+            active_entry = entry
+            break
+    return render_template("about.html", active_entry=active_entry)
--- factory.py
+++ factory.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from blueprints.home import home_bp
 from blueprints.about import about_bp
 from blueprints.contact import contact_bp
+from blueprints.cms import cms_bp
 
 
 def create_app():
@@ -12,6 +13,7 @@ def create_app():
     app.register_blueprint(home_bp)
     app.register_blueprint(about_bp)
     app.register_blueprint(contact_bp)
+    app.register_blueprint(cms_bp)
 
     @app.route("/", methods=["GET"])
     def index():
