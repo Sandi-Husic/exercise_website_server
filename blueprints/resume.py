6f5316920197f7c32696be39fccbd1ba3d0d637f
HardoMX <99422839+HardoMX@users.noreply.github.com>
2023-10-11 11:03:09 +0200

    Revert "Add resume and portfolio"

--- blueprints/resume.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-resume_bp = Blueprint("resume", __name__)
-
-
-@resume_bp.route("/resume", methods=["GET"])
-def resume():
-    return render_template("portfolio.html")
